11ac75894f5325e4e13422ad86356e41b26fc809
Anastasia <z.anastasia11@gmail.com>
2023-02-14 16:51:47 -0800

    add timer with beeping

---
+++ timer1.js
@@ -0,0 +1,16 @@
+const args = process.argv.slice(2);
+const timer = (args) => {
+  for (const arg of args) {
+    if (arg > 0 && !isNaN(arg)) {
+     
+      setTimeout(() => {
+        process.stdout.write(`\x07${arg}`);
+      }, arg );
+    }
+  }
+
+}
+
+timer([10, 3, 5, 15, 9])
+
+
